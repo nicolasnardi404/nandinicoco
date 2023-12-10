2f9dd9feb851d80e6457f621e8e31f48000a4cf8
nicolasnardi404 <151576395+nicolasnardi404@users.noreply.github.com>
2023-12-10 18:30:16 +0100

    create new poems/pages

--- pages/poetry.js
+++ pages/poetry.js
@@ -16,10 +16,31 @@ function updateContent(element) {
         <a href="./poetry/pt/copia.html">copia</a> 
     </li>
     <li> 
-        <a class="small-font" href="./poetry/pt/falar-de-amor.html">pra falar de amor<//a> 
+        <a class="small-font" href="./poetry/pt/falar-de-amor.html">pra falar de amor</a> 
     </li>
     <li> 
       <a href="./poetry/pt/cogumelos.html">cogumelos</a> 
+    </li>
+    <li> 
+    <a class="small-font" href="./poetry/pt/o-que.html">o que que eu sou</a> 
+    </li>
+    <li> 
+        <a href="./poetry/pt/agrotoxico.html">agrotoxico</a> 
+    </li>
+    <li> 
+        <a class="small-font" href="./poetry/pt/morre-humano.html">morre o ser humano</a> 
+    </li>
+    <li> 
+      <a href="./poetry/pt/presidente.html">presidente</a> 
+    </li>
+    <li> 
+        <a class="small-font" href="./poetry/pt/viver-uma-ideia.html">viver eh uma ideia</a> 
+    </li>
+    <li> 
+        <a href="./poetry/pt/poeta.html">poeta</a> 
+    </li>
+    <li> 
+        <a class="small-font" href="./poetry/pt/jesus.html">e se jesus voltasse</a> 
     </li>`;
   } else if (language === "en") {
     content.innerHTML = `<ul>
@@ -40,6 +61,24 @@ function updateContent(element) {
     </li>
     <li> 
         <a href="./poetry/en/mushrooms.html">mushrooms</a> 
+    </li>
+    <li> 
+        <a class = "small-font" href="./poetry/en/forget-the-heart.html">forget the heart outside the fridge</a> 
+    </li>
+    <li> 
+        <a href="./poetry/en/poetry.html">poetry</a> 
+    </li>
+    <li> 
+        <a class = "small-font" href="./poetry/en/notes.html">notes about artist funeral</a> 
+    </li>
+    <li> 
+        <a href="./poetry/en/war.html">war is phallic</a> 
+    </li>
+    <li> 
+        <a class = "small-font" href="./poetry/en/talk-about-love.html">to talk about love</a> 
+    </li>
+    <li> 
+        <a href="./poetry/en/machine.html">machine</a> 
     </li>`;
   }
 }
